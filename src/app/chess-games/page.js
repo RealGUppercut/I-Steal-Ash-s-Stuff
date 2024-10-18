d302b482e72f7abeed725e689af0e8b223c28158
Ashir-Valjee <ashir@eco-motive.com>
2024-10-18 13:34:44 +0100

    added sorting

--- src/app/chess-games/page.js
+++ src/app/chess-games/page.js
@@ -4,15 +4,78 @@
 import { db } from "@/utils/dbConnection";
 import Link from "next/link";
 
-export default async function ChessGamesPage() {
+export default async function ChessGamesPage({ searchParams }) {
   const games = await db.query(`SELECT * FROM chess_games`);
-  const data = games.rows;
+  let data = games.rows;
 
-  console.log(data);
+  // Get all values for the property 'opening'
+  const openings = data.map((obj) => obj.opening);
+
+  // Filter out the duplicates
+  const uniqueOpenings = [...new Set(openings)];
+
+  function handleChange(event) {
+    const value = e.target.value;
+    console.log(value);
+    // if (searchParams.category === value) {
+    //   data = data.filter((obj) => obj.opening === "Queens Gambit");
+    // }
+  }
+
+  // console.log(data);
+  console.log({ searchParams });
+  if (searchParams.sort === "asc") {
+    data.sort((a, b) => a.opening.localeCompare(b.opening));
+    // console.log(data.sort((a, b) => a.opening.localeCompare(b.opening)));
+  } else if (searchParams.sort === "desc") {
+    data.sort((a, b) => b.opening.localeCompare(a.opening));
+    // console.log(data.sort((a, b) => b.opening.localeCompare(a.opening)));
+  }
+
+  // if (searchParams.category === "Queens Gambit") {
+  //   data = data.filter((obj) => obj.opening === "Queens Gambit");
+  // }
+
+  uniqueOpenings.map((opening) => {
+    if (searchParams.category === opening) {
+      data = data.filter((obj) => obj.opening === opening);
+    }
+  });
+
+  // console.log(data.reverse());
+  // console.log(temp);
+  // const temp = data.sort((a, b) => b.opening.localeCompare(a.opening));
+  // console.log(temp);
+  console.log(uniqueOpenings);
 
   return (
     <>
-      <h1>Chess Games {data[0].white}</h1>
+      <h1>Chess Games </h1>
+      <div className="flex flex-row gap-6">
+        <Link href="/chess-games?sort=asc">Sort ascending</Link>
+        <Link href="/chess-games?sort=desc">Sort descending</Link>
+        <Link href="/chess-games?category=Queens Gambit">
+          Filter Queens gambit games
+        </Link>
+
+        <select>
+          <option value="">Select a category</option>
+          {uniqueOpenings.map((opening, index) => (
+            <option key={index} value={opening}>
+              {opening}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="flex flex-row gap-6 items-center">
+        <h2>Filter by Opening&#58; </h2>
+        {uniqueOpenings.map((opening, index) => (
+          <Link key={index} href={`/chess-games?category=${opening}`}>
+            {opening}
+          </Link>
+        ))}
+      </div>
+
       <div className="flex flex-col gap-4 m-4">
         {data.map((game) => (
           <div key={game.id}>
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <>
-      <h1>Home Page</h1>
+      <h1>A website about chess games</h1>
     </>
   );
 }
